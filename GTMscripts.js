8400508d8f8547912714a4cf869efbb13c7fc671
Marc Miguet <miguet.marc@gmail.com>
2023-12-04 21:15:53 +0100

    add_to_cart dataLayer set added

--- GTMscripts.js
+++ GTMscripts.js
@@ -1,15 +1,17 @@
+var room_ids = {
+    '8490': 'Barclay',
+    '8491': 'Orientale',
+    '8492': 'Louisiane',
+    '8493': 'Suite Junior',
+    '8494': 'Lodge',
+    '8495': 'Pavillon',
+    '21546': 'Ty Nid',
+    '9059056555': 'Carte cadeau 100€',
+    '9053164897': 'Carte cadeau 150€',
+};
+
 bookingRoomDataLayer = function() {
     var items = [];
-    var room_ids = {
-        '8490': 'Barclay',
-        '8491': 'Orientale',
-        '8492': 'Louisiane',
-        '8493': 'Suite Junior',
-        '8494': 'Lodge',
-        '8495': 'Pavillon',
-        '21546': 'Ty Nid',
-    };
-
     $('form.room_booked').each(function(i, elt) { 
         var item= {}; 
         item.item_id = ''+$('input#room_booked_room_id', $(elt)).attr('value'); 
@@ -19,7 +21,6 @@ bookingRoomDataLayer = function() {
         item.price = $('input#room_booked_total_price', $(elt)).attr('value'); 
         item.promotion_id = $('input#room_booked_pricing_type_id', $(elt)).attr('value'); 
         items[i] = item;
-        console.log(item); 
     });
 
     dataLayer.push({
@@ -31,3 +32,44 @@ bookingRoomDataLayer = function() {
         }
     });
 };
+
+/**
+ * set dataLayer on formSubmit
+ */
+$( document ).ready(function() {
+	$('form').submit(function(e) { 
+//		e.preventDefault(); 
+        var f = $(e.target[0]).parents('form');
+        var evtName = 'add_to_cart';
+        var item= { price: 0 }; 
+
+        if (f.attr('action').includes('add-room-to-cart')) {
+            item.item_id = ''+$('input#room_booked_room_id', f).attr('value'); 
+            item.item_name = room_ids[item.item_id]; 
+            item.item_category = $('input#room_booked_arrival_date', f).attr('value')+'-'+$('input#room_booked_departure_date', f).attr('value')+'/'+
+                                 $('input#room_booked_total_adult', f).attr('value')+' adultes-'+$('input#room_booked_total_children', f).attr('value')+' enfants'; 
+            item.price = $('input#room_booked_total_price', f).attr('value'); 
+            item.promotion_id = $('input#room_booked_pricing_type_id', f).attr('value'); 
+            item.item_list_id = $('input[name="cart_id"]', f).attr('value'); 
+                
+        } else if (f.attr('action').includes('add-gift-card-to-cart')) {
+            item.item_id = f.attr('action').match(/\d+/g)[0];
+            item.item_name = room_ids[item.item_id]; 
+            item.item_category = 'Carte Cadeau';
+            item.price = item.item_name.match(/\d+/g)[0];
+        } else {
+            evtName = '';
+        }
+
+        if (evtName) {
+            dataLayer.push({
+                event: evtName,
+                ecommerce: {
+                    currency: "EUR",
+                    value:  item.price,
+                    items: [item]
+                }
+            })
+        }
+    });    
+});
