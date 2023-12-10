f86b570216d20f53fa05bd8c485035030ce9ae52
Marc Miguet <miguet.marc@gmail.com>
2023-12-10 17:36:24 +0100

    add great tides table styling and js to open booking page

--- le1900.js
+++ le1900.js
@@ -29,4 +29,41 @@ $( document ).ready(function() {
 	// add booking cta
 	$('.site__navbar--side-div a').clone().addClass('hidden-md').appendTo('.room-model-2__desc, .room-model-2__amenities')
 
+	// grandes marées table hover and click to booking page
+	$('.pricing_table_block td').on("mouseenter", function(e) {
+		$(this).addClass('highlight');
+		var t = $(this).parents('table');
+		$('td[row-coordinate="'+$(this).attr('row-coordinate')+'"][column-coordinate="1"]', t).addClass('highlight');
+		$('td[column-coordinate="'+($(this).attr('column-coordinate') - $(this).attr('column-coordinate')%2)+'"][row-coordinate="1"]', t).addClass('highlight');
+		$('td[column-coordinate="'+$(this).attr('column-coordinate')+'"][row-coordinate="2"]', t).addClass('highlight');
+	}).on("mouseleave", function(e) {
+		$(this).removeClass('highlight');
+		var t = $(this).parents('table');
+		$('td[row-coordinate="'+$(this).attr('row-coordinate')+'"][column-coordinate="1"]', t).removeClass('highlight');
+		$('td[column-coordinate="'+($(this).attr('column-coordinate') - $(this).attr('column-coordinate')%2)+'"][row-coordinate="1"]', t).removeClass('highlight');
+		$('td[column-coordinate="'+$(this).attr('column-coordinate')+'"][row-coordinate="2"]', t).removeClass('highlight');
+	});
+
+	$('.pricing_table_block td').on("click", function(e) {
+		// if not on grades marees, return
+		if ($('#block_548444').length == 0) {
+			return;
+		}
+		// get day in 1st col
+		var d = $('td[row-coordinate="'+Math.max($(this).attr('row-coordinate'), 3)+'"][column-coordinate="1"]', $(this).parents('table')).text();
+		// get month in title above table
+		var m = $('h2', $(this).parents('.pricing_table_wrapper').parent()).text();
+
+		var dt = new Date(dt+' '+m);
+		// get language
+		var m = window.location.pathname.match(/\/[a-z]{2}\//);
+
+		// make url to booking page for selected date
+		var url = window.location.origin + (m ? m[0] : '/') + 'booking/room?info[arrival_date]='+dt.toLocaleDateString("fr");
+		dt.setDate((dt.getDate()+1));
+		url += '&info[departure_date]='+dt.toLocaleDateString("fr")+'&info[total_adult]=2';
+
+		// open url
+		window.open(url, '_blank');
+	});
 });
